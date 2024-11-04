e82f5be34864b7887e9ac0b29f8ab362343e5d0d
zickb <38587389+zickb@users.noreply.github.com>
2024-11-05 00:11:05 +0100

    Update buffer_previewer.lua

--- lua/telescope/previewers/buffer_previewer.lua
+++ lua/telescope/previewers/buffer_previewer.lua
@@ -585,7 +585,7 @@ previewers.vimgrep = defaulter(function(opts)
           jump_to_line(self, self.state.bufnr, entry)
         end)
       else
-        assert(nil, 'test telescope branch 2: ' .. tostring(entry.bufnr) .. ' - ' .. tostring(has_buftype))
+        assert(nil, 'test telescope branch 2: ' .. tostring(entry))
         conf.buffer_previewer_maker(p, self.state.bufnr, {
           bufname = self.state.bufname,
           winid = self.state.winid,

b8fc681c067767199e73b0710cd3ac34cbd5479d
zickb <38587389+zickb@users.noreply.github.com>
2024-11-07 18:49:01 +0100

    Update async_static_finder.lua

--- lua/telescope/finders/async_static_finder.lua
+++ lua/telescope/finders/async_static_finder.lua
@@ -15,7 +15,7 @@ return function(opts)
   local results = {}
   for k, v in ipairs(input_results) do
     local entry = entry_maker(v)
-    if entry.index == 1 then
+    if k == 1 then
       assert(nil, 'test telescope: ' .. vim.inspect(entry))
     end
     if entry then
